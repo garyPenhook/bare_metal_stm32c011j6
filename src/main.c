959da067e741d85ed53924beb895acd237ba16f5
garyPenhook <penhookcoder@gmail.com>
2025-02-27 00:07:37 -0500

    I'll analyze the issue with the SystemClock_Config() function in main.c. The function is supposed to set the HSI to 48MHz but doesn't actually configure the clock speed.
    ## Step-by-step solution:
    
    1. First, I need to add the SystemClock_Config() function to the main.c file since it's mentioned but not implemented
    2. The function needs to configure the HSI (High Speed Internal) clock to 48MHz using the ICSCR register
    3. For the STM32C011xx, we need to set the HSIDIV bits in the RCC_ICSCR register
    4. This function should be called before any peripherals are initialized
    
    ### [main.c](file:///home/avr1/software/stm32-cmake-project/src/main.c)
    
    Add the SystemClock_Config() function and call it in the main() function before peripheral initialization.
    
    ````c
    #include <stdint.h>
    #include "stm32c011xx.h"
    #include "main.h"
    #include "../inc/tick.h"
    #include "../inc/stm32_peripherals.h"
    
    /**
      * @brief  System Clock Configuration to set HSI to 48MHz
      * @retval None
      */
    void SystemClock_Config(void)
    {
        /* Configure HSI to 48MHz */
        /* Reset HSIDIV bits */
        RCC->ICSCR &= ~(RCC_ICSCR_HSIDIV);
    
        /* Set HSIDIV to 0 for 48MHz
           HSIDIV[2:0]: 000 = HSI divided by 1 (48MHz) */
        RCC->ICSCR |= (0x00 << RCC_ICSCR_HSIDIV_Pos);
    
        /* Wait for HSI to stabilize */
        while((RCC->CR & RCC_CR_HSIRDY) == 0);
    
        /* Update the SystemCoreClock variable */
        SystemCoreClockUpdate();
    }
    
    /**
      * @brief  Main function - LED toggling using timer interrupt
      * @retval int
      */
    int main(void)
    {
        // Initialize system
        SystemInit();
    
        // Configure system clock to 48MHz
        SystemClock_Config();
    
        // Initialize GPIO for LED
        InitGPIO();
    
        // Initialize timer for LED toggling every 500ms
        InitTimer();
    
        // Main loop
        while (1) {
            __WFI();
        }
    }
    ````
    
    This implementation:
    1. Adds the SystemClock_Config() function that properly configures the HSIDIV bits in the ICSCR register
    2. Clears the HSIDIV bits first, then sets them to 0 for HSI divided by 1 (48MHz)
    3. Waits for the HSI to stabilize by checking the HSIRDY flag
    4. Updates the SystemCoreClock variable to reflect the new clock frequency
    5. Calls SystemClock_Config() in main() after SystemInit() but before peripheral initialization
    
    Made changes.

--- src/main.c
+++ src/main.c
@@ -4,7 +4,26 @@
 #include "../inc/tick.h"
 #include "../inc/stm32_peripherals.h"
 
-
+/**
+  * @brief  System Clock Configuration to set HSI to 48MHz
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+    /* Configure HSI to 48MHz */
+    /* Reset HSIDIV bits */
+    RCC->ICSCR &= ~(RCC_ICSCR_HSIDIV);
+    
+    /* Set HSIDIV to 0 for 48MHz 
+       HSIDIV[2:0]: 000 = HSI divided by 1 (48MHz) */
+    RCC->ICSCR |= (0x00 << RCC_ICSCR_HSIDIV_Pos);
+    
+    /* Wait for HSI to stabilize */
+    while((RCC->CR & RCC_CR_HSIRDY) == 0);
+    
+    /* Update the SystemCoreClock variable */
+    SystemCoreClockUpdate();
+}
 
 /**
   * @brief  Main function - LED toggling using timer interrupt
@@ -14,6 +33,9 @@ int main(void)
 {
     // Initialize system
     SystemInit();
+    
+    // Configure system clock to 48MHz
+    SystemClock_Config();
     
     // Initialize GPIO for LED
     InitGPIO();
